fccfe563971bf15a545a0e138a3d4efefb656ecd
mhashim6 <m6hashim@gmail.com>
2020-05-24 04:57:23 +0200

    create / reply tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,52 @@
+import { showLoading, hideLoading } from 'react-redux-loading'
+import { saveLikeToggle, saveTweet } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
 
 export const receiveTweets = tweets => ({
     type: RECEIVE_TWEETS,
     tweets,
 })
 
+const toggleTweet = ({ id, authedUser, hasLiked }) => ({
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+})
+
+export const handleToggleTweet = tweet => dispatch => {
+    dispatch(toggleTweet(tweet))
+    saveLikeToggle(tweet).catch(() => {
+        console.warn('Error in: handleToggleTweet')
+        dispatch(toggleTweet(tweet))
+        alert('There was an error liking the tweet. Try again!')
+    })
+}
+
+const addTweet = tweet => ({
+    type: ADD_TWEET,
+    tweet
+})
+
+export const handleAddTweet = ({ text, replyingTo }) => (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    showLoading()
+
+    saveTweet({
+        text,
+        author: authedUser,
+        replyingTo,
+    }).then(savedTweet => {
+        dispatch(addTweet(savedTweet))
+    }).then(() => hideLoading())
+        .catch(e => {
+            console.error(e);
+            console.warn('Error in: handleAddTweet')
+            alert('There was an error saving the tweet. Try again!')
+            hideLoading()
+        })
+}
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,8 +1,32 @@
-import { RECEIVE_TWEETS } from "../actions/tweets"
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from "../actions/tweets"
 
 const tweets = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_TWEETS: return { ...state, ...action.tweets }
+        case TOGGLE_TWEET: {
+            const likedTweet = state[action.id]
+            return {
+                ...state, [action.id]: {
+                    ...likedTweet,
+                    likes: action.hasLiked // has already like the tweet
+                        ? likedTweet.likes.filter(id => id !== action.authedUser)
+                        : likedTweet.likes.concat(action.authedUser)
+                }
+            }
+        }
+        case ADD_TWEET:
+            const { tweet } = action
+            let parent = {}
+            if (tweet.replyingTo !== null) {
+                const currentParent = state[tweet.replyingTo]
+                parent = {
+                    [tweet.replyingTo]: {
+                        ...currentParent,
+                        replies: currentParent.replies.concat(tweet.id)
+                    }
+                }
+            }
+            return { ...state, [tweet.id]: tweet, ...parent }
         default: return state
     }
 }
